59194fda0b312eea332ddfe2294790da0e067af8
itu-itis23-sahinemi22 <sahinemi22@itu.edu.tr>
2024-06-30 13:26:54 +0300

    frequency.py updateed

--- frequency.py
+++ frequency.py
@@ -1,25 +1,52 @@
+import queue
+import sys
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
 import numpy as np
 import sounddevice as sd
 
-# Ses verisini alacak parametreler
-duration = 5  # kayıt süresi (örneğin 5 saniye)
-samplerate = 44100  # örnekleme hızı
+device = 0 # id of the audio device by default
+window = 1000 # window for the data
+downsample = 1 # how much samples to drop
+channels = [1] # a list of audio channels
+duration = 5
 
+samplerate = sd.query_devices(device, 'input')['default_samplerate']
+
+length  = int(window*samplerate/(500*downsample))
 # Ses verisini al
-print("Ses kaydı başlıyor. Lütfen konuşun...")
-recording = sd.rec(int(duration * samplerate), samplerate=samplerate, channels=1, dtype='float64')
-sd.wait()
 
-# Fourier dönüşümü uygula
-signal = recording.flatten()  # ses verisini düzleştir
-fft_result = np.fft.fft(signal)  # Fourier dönüşümü uygula
-freqs = np.fft.fftfreq(len(fft_result), 1/samplerate)  # frekans bileşenlerini hesapla
+freq_data = []
+data = 0
+def update_plot(frame):
+    
+    try:    
+        data = recording = sd.rec(int(duration * samplerate), samplerate=samplerate, channels=1, dtype='float64')
+        sd.wait()
+    except(sd.PortAudioError):
+        data = np.zeros((length,len(channels)))
+    signal = data.flatten()  # ses verisini düzleştir
+    fft_result = np.fft.fft(signal)  # Fourier dönüşümü uygula
+    freqs = np.fft.fftfreq(len(fft_result), 1/samplerate)  # frekans bileşenlerini hesapla
+    
+    power = np.abs(fft_result)
+    dominant_freq = freqs[np.argmax(power)]
+    print(f"En baskın frekans bileşeni: {dominant_freq} Hz")
+    freq_data.append(dominant_freq)
+    
+    ax.clear()
+    ax.plot(freq_data)
+    print(freq_data)
 
-# En yüksek güce sahip frekans bileşenini bul
-power = np.abs(fft_result)
-dominant_freq = freqs[np.argmax(power)]
+fig,ax = plt.subplots(figsize=(10,10))
 
-print(f"En baskın frekans bileşeni: {dominant_freq} Hz")
+
+
+animation = FuncAnimation(fig, update_plot, interval=duration*1000)
+
+plt.show()
+
+
 
 '''Örnek Frekans Değerleri
 Do (C4) notası: Yaklaşık 261.63 Hz
