4f547eefec2e8f7b833b456bbed1b4dbd9edbbe7
itu-itis23-sahinemi22 <sahinemi22@itu.edu.tr>
2024-07-06 12:16:12 +0300

    improvement

--- frequency.py
+++ frequency.py
@@ -9,44 +9,56 @@ device = 0 # id of the audio device by default
 window = 1000 # window for the data
 downsample = 1 # how much samples to drop
 channels = [1] # a list of audio channels
-duration = 5
+duration = 1
 
 samplerate = sd.query_devices(device, 'input')['default_samplerate']
 
 length  = int(window*samplerate/(500*downsample))
 # Ses verisini al
 
-freq_data = []
-data = 0
-def update_plot(frame):
-    
-    try:    
-        data = recording = sd.rec(int(duration * samplerate), samplerate=samplerate, channels=1, dtype='float64')
-        sd.wait()
-    except(sd.PortAudioError):
-        data = np.zeros((length,len(channels)))
-    signal = data.flatten()  # ses verisini düzleştir
-    fft_result = np.fft.fft(signal)  # Fourier dönüşümü uygula
-    freqs = np.fft.fftfreq(len(fft_result), 1/samplerate)  # frekans bileşenlerini hesapla
-    
+fig, ax = plt.subplots(figsize=(10, 5))
+
+line, = ax.plot([], [], lw=2)
+ax.set_ylim(0, 600)
+xdata, ydata = [], []
+
+def init():
+    line.set_data([], [])
+    return line,
+
+
+def audio_callback(indata, frames, time, status):
+    if status:
+        print(status)
+    signal = indata[:, 0]
+    fft_result = np.fft.fft(signal)
+    freqs = np.fft.fftfreq(len(fft_result), 1 / samplerate)
     power = np.abs(fft_result)
     dominant_freq = freqs[np.argmax(power)]
-    print(f"En baskın frekans bileşeni: {dominant_freq} Hz")
-    freq_data.append(dominant_freq)
     
-    ax.clear()
-    ax.plot(freq_data)
-    print(freq_data)
+    if 0 < dominant_freq < 600:
+        xdata.append(len(xdata))
+        ydata.append(dominant_freq)
+        
+        if len(xdata) > 100:
+            ax.set_xlim(len(xdata) - 100, len(xdata))
+            
+        else:
+            ax.set_xlim(0, 100)
+        
+        line.set_data(xdata, ydata)
+    
+def update_plot(frame):
+    return line,
 
-fig,ax = plt.subplots(figsize=(10,10))
+stream = sd.InputStream(callback=audio_callback, channels=1, samplerate=samplerate)
+stream.start()
 
-
-
-animation = FuncAnimation(fig, update_plot, interval=duration*1000)
-
+animation = FuncAnimation(fig, update_plot,init_func=init, interval=1000,blit = True)
 plt.show()
 
-
+stream.stop()
+stream.close()
 
 '''Örnek Frekans Değerleri
 Do (C4) notası: Yaklaşık 261.63 Hz
