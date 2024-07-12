a8eb9fb194769bbc1d2ce5e1aaaa9aaa4f03f112
itu-itis23-sahinemi22 <sahinemi22@itu.edu.tr>
2024-07-12 12:52:48 +0300

    improvements on plotting

--- frequency_updated.py
+++ frequency_updated.py
@@ -5,23 +5,31 @@ import numpy as np  # for using np arrays
 import sounddevice as sd  # for getting sound as input
 
 device = 0  # id of the audio device by default
-window = 1000  # window for the data
-downsample = 1  # how much samples to drop
 channels = [1]  # a list of audio channels
-interval = 30  # time interval for plot update
-freq_counter = 100  # is limit of x axis
+interval = 0  # time interval for plot update
+x_limit = 250  # is limit of x axis
 
-samplerate = sd.query_devices(device, 'input')['default_samplerate']
+samplerate = sd.query_devices(device, 'input')['default_samplerate'] #setting samplerate as default of sound device
 
 q = queue.Queue()  # for creating queues to get voice data in order
 
-freq_data = np.zeros((1, freq_counter))
+freq_data = np.zeros([])
 # will be y_axis in plot, contains dominant frequency data
 
-fig, ax = plt.subplots(figsize=(10, 5))  # fig and axis in plot
+fig, ax = plt.subplots(figsize=(15, 5))  # fig and axis in plot
+
 plt.ylim(0, 600)  # we want freqs between 0-600
+plt.xlim(0,x_limit) 
+plt.ylabel('Frequency (Hz)') # label of axis y
+
+left_top_text = fig.text(0.10, 1, "press c to clear plot\npress p to pause and continue plot", ha='left', va='top', fontsize=10, color='blue')
+right_top_text = fig.text(0.70, 1, "press right arrow button to move plot right\n press left arrow button to move plot left\n \
+press z to move to x = 0", ha='left', va="top", fontsize=10, color="blue")
+# explanations about keys
+
 
 line, = ax.plot([], [], 'r-')
+
 
 is_paused = False  # Global variable to track the pause state
 
@@ -48,7 +56,8 @@ def update_plot(frame):
     global i
     global freq_data
 
-    if is_paused:  # Skip updating if paused
+    if is_paused:# Skip updating if paused
+        line.set_data(np.arange(i), freq_data[ :i])# keep updating line to be able to clear plot when paused
         return line,
 
     while True:
@@ -56,31 +65,40 @@ def update_plot(frame):
             data = q.get_nowait()  # getting dominant_freqs while there are in queue
         except queue.Empty:
             break  # if queue is empty, break and return line
-        shift = 1  # shift = 1 since len(data) is always equal to 1
-
-        # i < freq_counter means there is no rolling, data fits in first plot
-        if i < freq_counter:
-            freq_data[0, i] = data[0]  # adding first <freq_counter> data
-            ax.set_xlim(0, freq_counter)
-            line.set_data(np.arange(i), freq_data[0, :i])
-        # if not, we have to roll to add new data in plot
-        else:
-            freq_data = np.roll(freq_data, -shift, axis=1)  # rolling freq_data array by 1
-            freq_data[0, freq_counter - 1] = data[0]  # changing last data in array
-            ax.set_xlim(i - freq_counter, i)
-            line.set_data(np.arange(i - freq_counter, i), freq_data[0])
+    
+        freq_data = np.append(freq_data, data[0])  # adding data
+        line.set_data(np.arange(i), freq_data[ :i]) # setting data into line
         i += 1
     return line,
 
 def toggle_pause(event):
     global is_paused
-    if event.key == 'z':
+    global i
+    
+    if event.key == 'p': # if <key> is pressed, stop or continue
         is_paused = not is_paused
-        if is_paused:
-            print("Paused")
-        else:
-            print("Resumed")
-
+    
+def clear_plot(event):
+    global i
+    global freq_data
+    if event.key == 'c': 
+        freq_data = np.array([])# clearing array
+        i = 0 # clearing x axis
+        ax.set_xlim(0,x_limit) # for moving the plot 
+        fig.canvas.draw() # to x = 0
+def move_plot(event):
+    global i
+    xlim = ax.get_xlim() 
+    step = 0.1 * (xlim[1] - xlim[0]) # moving 1/10 of current x_limit at a time
+    if (event.key == 'right' or event.key == 'd'):
+        ax.set_xlim(xlim[0] + step, xlim[1] + step) # moving plot right
+        fig.canvas.draw()
+    elif (event.key == 'left' or event.key == 'a'):
+        ax.set_xlim(xlim[0] - step, xlim[1] - step) # moving plot left
+        fig.canvas.draw()
+    elif (event.key == 'z'):
+        ax.set_xlim(0,x_limit) # moving plot to x = zero
+        fig.canvas.draw()
 # sound input
 stream = sd.InputStream(device=device, channels=max(channels), samplerate=samplerate, callback=audio_callback)
 
@@ -88,6 +106,8 @@ stream = sd.InputStream(device=device, channels=max(channels), samplerate=sample
 animation = FuncAnimation(fig, update_plot, init_func=init, interval=interval, blit=True)
 
 fig.canvas.mpl_connect('key_press_event', toggle_pause)
+fig.canvas.mpl_connect('key_press_event', clear_plot)
+fig.canvas.mpl_connect('key_press_event', move_plot)
 
 with stream:
     plt.show()
@@ -101,10 +121,4 @@ Fa (F4) notası: Yaklaşık 349.23 Hz
 Sol (G4) notası: Yaklaşık 392.00 Hz
 La (A4) notası: 440.00 Hz
 Si (B4) notası: Yaklaşık 493.88 Hz
-
-y eksenindeki 0-600 aralığı notalar şeklinde ayarlancak 
-
-plotun görünümü iyileştirilecek 
-
-
 '''
